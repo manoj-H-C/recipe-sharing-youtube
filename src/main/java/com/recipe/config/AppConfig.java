65e6dabcd0679b03d529fef902bcf32c29826946
manoj-H-C <manojhc110@gmail.com>
2024-09-18 23:55:36 +0530

    jwt-security changes

--- src/main/java/com/recipe/config/AppConfig.java
+++ src/main/java/com/recipe/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -22,6 +23,7 @@ public class AppConfig {
         )).authorizeHttpRequests(
                 Authorize -> Authorize.requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 .csrf(csrf ->csrf.disable())
                 .cors(cors->cors.configurationSource(corsConfigurationSource())).httpBasic(Customizer.withDefaults()).formLogin(Customizer.withDefaults());
         return http.build();
---
+++ src/main/java/com/recipe/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.recipe.config;
+
+public class JwtConstant {
+    public static final String JWT_SECRET="kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk";
+    public static final String JWT_HEADER="Authorization";
+}
